51d926351d88f5e07194d0c2e87a7804c8dc7bfa
Colin Zhou <colinzhou@berkeley.edu>
2020-02-15 19:59:14 -0800

    v1

---
+++ Pokedex/View/PokedexCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PokedexCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Colin Zhou on 2020-02-13.
+//  Copyright © 2020 Colin Zhou. All rights reserved.
+//
+
+import UIKit
+
+class PokedexCollectionViewCell: UICollectionViewCell {
+    
+
+    
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var pokemonID: UILabel!
+    
+    
+}
